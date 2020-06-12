85a1d1390e0bfbb21e250fe2a6ade9a6e9d5eeb1
fadelatoz <fadelalatas81@gmail.com>
2020-06-12 18:34:59 +0700

    Updates

---
+++ precache-manifest.7ec0e1b58ac9c96a29ed75d2f440efe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759849f7b4fa4d678e26b084c3ace377",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8bc3461fffe73414fba",
+    "url": "/static/css/main.072d3ff7.chunk.css"
+  },
+  {
+    "revision": "01ef48a62623ab652fa3",
+    "url": "/static/js/2.e7a679d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7a679d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bc3461fffe73414fba",
+    "url": "/static/js/main.e196678f.chunk.js"
+  },
+  {
+    "revision": "d2a5255914fef611900a",
+    "url": "/static/js/runtime-main.138aa97e.js"
+  },
+  {
+    "revision": "7110d768bc25a142a4684ae6bb4ba96f",
+    "url": "/static/media/Fadel Foto.7110d768.jpg"
+  }
+]);
