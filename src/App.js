209c024dc0595742cccfdf8fc3678af3dfdbb323
fadelatoz <fadelalatas81@gmail.com>
2020-06-12 10:21:57 +0700

    Latihan React js
    ini latihan react js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import 'CSS/App.scss';
+
+class App extends Component{
+  render(){
+    return(
+      <div class="app">
+       <center> <h1>Hello world Fadel</h1>
+
+        <div class="biodata">
+        <h1>Biodata</h1>
+        <h3>Nama : Mohamad Fadel</h3>
+        <h3>umur : 23 tahun </h3>
+        <h3>jurusan : Sistem Informasi</h3>
+        <h3>universitas : Universitas Gunadarma</h3>
+        
+
+        </div>
+
+        </center>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
